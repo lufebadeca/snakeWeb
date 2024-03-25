d5fb384f7c39adbac56ac2e82b7b915b5a363966
lufebadeca <lufebadeca@gmail.com>
2024-03-25 18:06:13 -0500

    limits set at borders and reverse direction blocked

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameOver=false;
 let foodX, foodY;
 let snakeX = 10, snakeY = 10;
 let snakeBody = []; // [ [Xhead,Yhead], [X1,Y1] ]
 let velocityX = 0, velocityY = 0;
+let setIntervalID;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30 ) + 1;
     foodY = Math.floor(Math.random() * 30 ) + 1;
 }
 
+const handleGameOver =() => {
+    clearInterval(setIntervalID); //clears the main flow interval storered at setIntervalID
+    alert("Game Over");
+}
+
 const changeDirection = (e) => {
     //console.log(e);
-    if(e.key == "ArrowUp"){
+    if(e.key == "ArrowUp" && velocityY!=1 ){
         velocityY = -1;
         velocityX = 0;
     }
-    if(e.key == "ArrowDown"){
+    if(e.key == "ArrowDown" && velocityY!=-1 ){
         velocityY = 1;
         velocityX = 0;
     }
-    if(e.key == "ArrowLeft"){
+    if(e.key == "ArrowLeft" && velocityX!=1 ){
         velocityY = 0;
         velocityX = -1;
     }
-    if(e.key == "ArrowRight"){
+    if(e.key == "ArrowRight" && velocityX!=-1 ){
         velocityY = 0;
         velocityX = 1;
     }
 }
 
 const initGame = () => {
+    if(gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX};"></div>`; 
 
     if(snakeX === foodX && snakeY=== foodY){    //if snake (head) touches food
@@ -48,6 +56,9 @@ const initGame = () => {
     snakeX += velocityX; 
     snakeY += velocityY;
     
+    if(snakeX <= 0 || snakeX>30 || snakeY <=0 || snakeY>30 ) {
+        gameOver=true;
+    }
 
     for(let i=0; i<snakeBody.length; i++ ){
         htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]};"></div>`;
@@ -58,7 +69,7 @@ const initGame = () => {
 }
 
 changeFoodPosition();
-setInterval(initGame, 130);
+setIntervalID = setInterval(initGame, 130);
 document.addEventListener("keydown", changeDirection);
 
 
